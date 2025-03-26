31720024214190f3598a0d6ae0ee57547be994e3
Christiane Kobalt <kobalt@login1.hsn.dardel.pdc.kth.se>
2025-03-26 14:41:48 +0100

    batch job now also runs on 128 cores

--- Assignment1/Task1/mpi_hello_world_batch_job.sh
+++ Assignment1/Task1/mpi_hello_world_batch_job.sh
@@ -1,18 +1,13 @@
 #!/bin/bash -l
 # The -l above is required to get the full environment with modules
 
-# The name of the script is myjob
 #SBATCH -J mpi_hello_world_batch_job 
-# Only 1 hour wall-clock time will be given to this job
-#SBATCH -t 1:00:00
+#SBATCH -t 00:02:00
 #SBATCH -A edu25.DD2356
 # Number of nodes
-#SBATCH -p shared
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=2
+#SBATCH -p main 
 #SBATCH --nodes=1
 #SBATCH -e error_file.e
 
 # Run the executable file 
-# and write the output into my_output_file
-srun -n 1 ./mpi_hello_world.out > mpi_hello_world_output
+srun -n 128 ./mpi_hello_world.out > mpi_hello_world_output
