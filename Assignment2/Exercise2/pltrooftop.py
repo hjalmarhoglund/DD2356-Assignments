7049d2ca2c295fb2e34038dfaf913f9156fa5e87
Christiane Kobalt <kobalt@login1.hsn.dardel.pdc.kth.se>
2025-04-14 17:27:57 +0200

    established roofline model

--- Assignment2/Exercise2/pltrooftop.py
+++ Assignment2/Exercise2/pltrooftop.py
@@ -2,16 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 operational_intensity = np.array([0.042, 0.042, 0.042, 0.042])  # FLOPs per byte
-performance = np.array([0.5, 0.68, 0.73, 0.40])  # GFLOP/s
+performance = np.array([0.5, 0.68, 0.73, 0.40]) * 10 ** 9 # FLOP/s
 #operational_intensity = np.array([0.1, 1, 10, 50])  # FLOPs per byte
-#performance = np.array([10, 50, 100, 150])  # GFLOP/s
+#performance = np.array([10, 50, 100, 150]) * 10 ** 9 # FLOP/s
+
+memory_bandwidth = 979.2149 * 10 ** 9 # bytes/s
+compute_peak = 1440 * 10 ** 9 # FLOP/s
+
+x_for_slope = np.pow(np.linspace(0.1,10,1000),10)
+
+memory_line = memory_bandwidth * x_for_slope
 
 plt.figure(figsize=(8,6))
 plt.loglog(operational_intensity, performance, marker='o', label='Measured Data')
-plt.axhline(y=200, color='r', linestyle='--', label='Peak FLOP/s')
-
-x_for_slope = np.pow(np.linspace(0.1,10,1000),10)
-plt.loglog(x_for_slope, x_for_slope, linestyle='dotted', color='gray', label='Memory Bandwidth Limit')
+plt.axhline(y=compute_peak, color='r', linestyle='--', label='Peak FLOP/s')
+plt.loglog(x_for_slope, memory_line, linestyle='dotted', color='gray', label='Memory Bandwidth Limit')
 plt.xlabel('Operational Intensity (FLOPs/Byte)')
 plt.ylabel('Performance (GFLOP/s)')
 plt.title('Roofline Model')
---
+++ Assignment2/Exercise2/run_stream.sh
@@ -0,0 +1,2 @@
+export OMP_NUM_THREADS=128
+srun --cpus-per-task=128 ./stream
