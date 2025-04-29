36aa7bef4d28372703466f70817d313833161886
Christiane Kobalt <kobalt@login1.hsn.dardel.pdc.kth.se>
2025-04-29 19:06:22 +0200

    ran stream benchmark with different number of threads

--- Assignment3/Exercise2/run_stream.sh
+++ Assignment3/Exercise2/run_stream.sh
@@ -1,2 +1,23 @@
-export OMP_NUM_THREADS=128
-srun --cpus-per-task=128 ./stream
+#!/bin/bash
+#SBATCH --account=edu25.dd2356
+#SBATCH --job-name=stream_bm
+#SBATCH --output=stream_bm_%j.out
+#SBATCH -p main
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+#SBATCH --time=00:20:00
+#SBATCH --mem=100G
+
+# compile
+gcc -O3 -fopenmp -DSTREAM_ARRAY_SIZE=20000000 stream.c -o stream
+
+threads=(1 32 64 128)
+
+for t in "${threads[@]}"; do
+  export OMP_NUM_THREADS=$t
+  > results_${t}.txt
+  for i in {1..5}; do
+    srun --cpus-per-task=$t ./stream | grep Copy: | awk '{print $2}' >> results_${t}.txt
+  done
+done
