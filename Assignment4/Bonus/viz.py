b175a4cb85fc9310ddd6306b630c5860dceceb92
hjalmarhoglund <hoglund.hjalmar@gmail.com>
2025-05-09 10:54:03 +0200

    Added visualization scripts from Canvas

---
+++ Assignment4/Bonus/viz.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+
+files = sorted(glob.glob("gol_output_*.txt"))
+
+for file in files:
+    data = np.loadtxt(file)
+    plt.imshow(data, cmap='binary', origin='lower')
+    plt.title(f"Game of Life - {file}")
+    plt.pause(0.5)
+
+plt.show()
