b95613ecb158fa459cd4c39b87a6e70265efe5a7
hjalmarhoglund <hoglund.hjalmar@gmail.com>
2025-05-14 12:07:09 +0200

    I think done

--- Assignment4/Exercise1/main.c
+++ Assignment4/Exercise1/main.c
@@ -1,12 +1,14 @@
 /*
 Authors: Christiane Kobalt & Hjalmar Höglund for DD2356 Assignment 4
 Date: 2025-05-09
- */
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <mpi.h>
+#include <assert.h>
 
 #define N 1000 // Grid size
 #define STEPS 100 // Time steps
@@ -16,49 +18,81 @@ Date: 2025-05-09
 
 double u[N], u_prev[N];
 
-void initialize() {
-    for (int i = 0; i < N; i++) {
-        u[i] = sin(2.0 * M_PI * i / N);
-        u_prev[i] = u[i];
-    }
+int max(int x, int y) {
+	if (x < y) return y;
+	return x;
 }
 
-void compute_step() {
-    double u_next[N];
-    for (int i = 1; i < N - 1; i++) {
-        u_next[i] = 2.0 * u[i] - u_prev[i] + C * C * DT * DT / (DX * DX) * (u[i+1] - 2.0 * u[i] + u[i-1]);
-    }
-    for (int i = 0; i < N; i++) {
-        u_prev[i] = u[i];
-        u[i] = u_next[i];
-    }
+int min(int x, int y) {
+	if (x < y) return x;
+	return y;
+}
+
+void initialize(int istart, int iend) {
+	for (int i = istart; i < iend; i++) {
+		u[i] = sin(2.0 * M_PI * i / N);
+		u_prev[i] = u[i];
+	}
+}
+
+void compute_step(int istart, int iend) {
+	double u_next[N];
+	for (int i = max(istart, 1); i < min(iend, N - 1); i++) {
+		u_next[i] = 2.0 * u[i] - u_prev[i] + C * C * DT * DT / (DX * DX) * (u[i+1] - 2.0 * u[i] + u[i-1]);
+	}
+	for (int i = istart; i < iend; i++) {
+		u_prev[i] = u[i];
+		u[i] = u_next[i];
+	}
 }
 
 void write_output(int step) {
-    char filename[50];
-    sprintf(filename, "wave_output_%d.txt", step);
-    FILE *f = fopen(filename, "w");
-    for (int i = 0; i < N; i++) {
-        fprintf(f, "%f\n", u[i]);
-    }
-    fclose(f);
+	char filename[50];
+	sprintf(filename, "wave_output_%d.txt", step);
+	FILE *f = fopen(filename, "w");
+	for (int i = 0; i < N; i++) {
+		fprintf(f, "%f\n", u[i]);
+	}
+	fclose(f);
 }
 
-int main(int argc, char** argv) {
-    int rank, nprocs;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-    printf("MPI rank %d out of %d processes.\n", rank, nprocs);
-
-    initialize();
-    for (int step = 0; step < STEPS; step++) {
-        compute_step();
-        if (step % 10 == 0) write_output(step);
-    }
-    MPI_Finalize();
-
-    if(rank==0)
-        printf("Simulation complete.\n");
-    return 0;
+void sync_arr(double* arr, int rank, int nprocs, int istart, int iend, int stride) {
+	for (int i = 0; i < nprocs; i++) {
+		if (i == rank) continue;
+		int ostart = i * stride;
+		int oend = (i+1) * stride;
+		if (i == nprocs - 1) oend = N;
+		MPI_Sendrecv(arr+istart, iend-istart, MPI_DOUBLE, i, 0,
+			     arr+ostart, oend-ostart, MPI_DOUBLE, i, 0,
+			     MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+	}
 }
+
+int main(int argc, char* argv[]) {
+	int rank, nprocs, provided;
+	MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+	printf("MPI rank %d out of %d processes.\n", rank, nprocs);
+	assert(nprocs > 1);
+	int stride = N / nprocs;
+	int istart = rank * stride;
+	int iend = (rank + 1) * stride;
+	if (rank == nprocs - 1) iend = N;
+
+	initialize(istart, iend);
+	sync_arr(&u[0], rank, nprocs, istart, iend, stride);
+	memcpy(&u_prev, &u, N);
+	for (int step = 0; step < STEPS; step++) {
+		compute_step(istart, iend);
+		sync_arr(&u_prev[0], rank, nprocs, istart, iend, stride);
+		sync_arr(&u[0], rank, nprocs, istart, iend, stride);
+		if (rank == 0 && step % 10 == 0) write_output(step);
+	}
+	MPI_Finalize();
+
+	if(rank==0)
+		printf("Simulation complete.\n");
+	return 0;
+}
+
