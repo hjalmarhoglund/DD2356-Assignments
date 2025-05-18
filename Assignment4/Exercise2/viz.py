50c5782302d1dc8f19f93275bb3822745edde3d4
hjalmarhoglund <hoglund.hjalmar@gmail.com>
2025-05-18 22:57:51 +0200

    Save to file instead of showing on screen

--- Assignment4/Exercise2/viz.py
+++ Assignment4/Exercise2/viz.py
@@ -2,9 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = np.loadtxt("row_sums_output.txt")
+plt.figure()
 plt.plot(data, marker='o', label='Row Sums')
 plt.xlabel("Row Index")
 plt.ylabel("Sum Value")
 plt.title("Parallel Row Sum Computation with MPI")
 plt.legend()
-plt.show()
+plt.savefig('par.png')
+
+data = np.loadtxt("ser_row_sums_output.txt")
+plt.figure()
+plt.plot(data, marker='o', label='Row Sums')
+plt.xlabel("Row Index")
+plt.ylabel("Sum Value")
+plt.title("Serial Row Sum Computation")
+plt.legend()
+plt.savefig('serial.png')
