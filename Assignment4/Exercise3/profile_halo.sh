fa7f5d5a25e406e6c725e851b31840ff212cf876
Christiane Kobalt <kobalt@login1.hsn.dardel.pdc.kth.se>
2025-05-18 14:54:27 +0200

    profile script

---
+++ Assignment4/Exercise3/profile_halo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --job-name=profile_halo
+#SBATCH --account=edu25.dd2356
+#SBATCH --partition=main
+#SBATCH --nodes=2             
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=00:05:00
+#SBATCH --output=prof_%j.txt
+
+module purge
+module load PrgEnv-gnu/8.5.0
+module load score-p/8.4-cpeGNU   
+
+scorep mpicc -O3 ex1_halo.c -o ex1_halo.out
+
+export SCOREP_ENABLE_PROFILING=true
+export SCOREP_ENABLE_TRACING=false
+
+srun ./ex1_halo.out
